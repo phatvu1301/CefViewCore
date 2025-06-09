5b4e232b77db7fc78a4cc26215b2b7280a94d4cd
Vu Huy phat <phatvhup@gmail.com>
2025-06-09 14:09:01 +0700

    update

--- include/CefViewCoreAssetBridge.h
+++ include/CefViewCoreAssetBridge.h
@@ -3,12 +3,16 @@
 
 #pragma once
 typedef const char* (*FnAsset)(const char*, int*, const char**);
+typedef void (*FnFree)(const char*);
 
 #ifdef __cplusplus
 extern "C"{
 #endif
   void SetAssetFunc(FnAsset func);
+  void SetFreeFunc(FnFree func);
   extern FnAsset g_asset_func;
+  extern FnFree g_free_func;
+
 #ifdef __cplusplus
 }
 #endif
--- src/CefView/CefBrowserApp/CefViewCoreAssetBridge.cpp
+++ src/CefView/CefBrowserApp/CefViewCoreAssetBridge.cpp
@@ -2,9 +2,16 @@
 #include "CefViewCoreAssetBridge.h"
 
 FnAsset g_asset_func = nullptr;
+FnFree g_free_func = nullptr;
 
 void
 SetAssetFunc(FnAsset func)
 {
   g_asset_func = func;
+}
+
+void
+SetFreeFunc(FnFree func)
+{
+  g_free_func = func;
 }
