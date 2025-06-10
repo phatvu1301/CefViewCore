d20f6bfa3c96ab307f4ade9b27ab3a68062141f1
Vu Huy phat <phatvhup@gmail.com>
2025-06-10 13:38:02 +0700

    update

--- src/CefView/CefBrowserApp/CefViewSchemeHandler/CustomCefViewSchemeHandler.cpp
+++ src/CefView/CefBrowserApp/CefViewSchemeHandler/CustomCefViewSchemeHandler.cpp
@@ -33,13 +33,18 @@ CustomCefViewSchemeHandler::Open(CefRefPtr<CefRequest> request, bool& handle_req
   std::string path;
   std::cout << "Using Custom Scheme - Open -2 " <<  url << std::endl;
 
-  size_t pos = url.find("://");
-  if (pos != std::string::npos) {
-    path = url.substr(pos + 3);
-  } else {
-    path = url;
-  }
-  while (!path.empty() && path.back() == '/') path.pop_back();
+  size_t schemePos = url.find("://");
+  std::string afterScheme = url.substr(schemePos + 3);
+
+  auto slash = afterScheme.find('/');
+  if (slash != std::string::npos)
+    afterScheme = afterScheme.substr(slash + 1);
+
+  while (!afterScheme.empty() && afterScheme.back() == '/')
+    afterScheme.pop_back();
+  if (afterScheme.empty()) afterScheme = "index.html";
+
+  path = afterScheme;
 
   std::cout << "Using Custom Scheme - Open - Path  " <<  path << std::endl;
 
